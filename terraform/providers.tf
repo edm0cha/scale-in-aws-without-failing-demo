54b5f2e14219e1d93340add8db8148032ff35fc2
Edwin Moedano <edwin.moedano@caylent.com>
2026-08-10 18:58:33 -0600

    feat: updated arn role

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -9,7 +9,7 @@ terraform {
 
   backend "s3" {
     bucket  = "shared-superlab-scale-in-aws-without-failing-demo-tfstate"
-    key     = "terraform.tfstate"
+    key     = "prod/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
   }
--- terraform/shared/providers.tf
+++ terraform/shared/providers.tf
@@ -4,11 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-    archive = {
-      source  = "hashicorp/archive"
-      version = "~> 2.4"
+      version = "~> 6.56"
     }
   }
 
