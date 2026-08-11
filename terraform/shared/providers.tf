29208c06eef2211a563dd2155758a19ec772aa8e
Edwin Moedano <edwin.moedano@caylent.com>
2026-08-10 18:54:39 -0600

    fix: s3 bucket state providers

--- terraform/shared/providers.tf
+++ terraform/shared/providers.tf
@@ -13,7 +13,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket  = "shared-superlab-awsugcdmx-tfstate"
+    bucket  = "shared-superlab-scale-in-aws-without-failing-demo-tfstate"
     key     = "shared/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
