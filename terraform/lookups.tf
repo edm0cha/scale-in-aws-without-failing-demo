192ceb9cc977c52ca9b8bc8eb1965a22852f4b0a
Edwin Moedano <edwin.moedano@caylent.com>
2026-08-11 16:51:28 -0600

    feat: updated lookups

--- terraform/lookups.tf
+++ terraform/lookups.tf
@@ -18,9 +18,27 @@ data "aws_vpc" "default" {
   default = true
 }
 
+# Not every AZ in a region supports every instance type (e.g. c5.large is
+# unavailable in us-east-1e on some accounts). Restrict subnets to AZs that
+# actually support the instance type we launch, so the ASG doesn't try to
+# place instances in an AZ that will fail.
+data "aws_ec2_instance_type_offerings" "available" {
+  filter {
+    name   = "instance-type"
+    values = [var.instance_type]
+  }
+
+  location_type = "availability-zone"
+}
+
 data "aws_subnets" "default" {
   filter {
     name   = "vpc-id"
     values = [data.aws_vpc.default.id]
+  }
+
+  filter {
+    name   = "availability-zone"
+    values = data.aws_ec2_instance_type_offerings.available.locations
   }
 }
