72e962533e984ca167e6c764f4a3088b77be238f
Edwin Moedano <edwin.moedano@caylent.com>
2026-08-10 18:50:32 -0600

    fix: shared infra integrated iam roles

--- terraform/shared/iam.tf
+++ terraform/shared/iam.tf
@@ -38,37 +38,8 @@ resource "aws_iam_policy" "management" {
   })
 }
 
-resource "aws_iam_policy" "static_content" {
-  name        = "${var.prefix}-${var.project_name}-static-content-github-actions"
-  description = "Permissions for the GitHub Actions pipeline to deploy S3 and CloudFront"
-  policy = templatefile("${path.root}/policies/static-content-github-actions.json", {
-    project_name = var.project_name
-  })
-}
-
-resource "aws_iam_policy" "backend" {
-  name        = "${var.prefix}-${var.project_name}-backend-github-actions"
-  description = "Permissions for the GitHub Actions pipeline to deploy S3 and CloudFront"
-  policy = templatefile("${path.root}/policies/backend-github-actions.json", {
-    account_id   = data.aws_caller_identity.current.account_id
-    project_name = var.project_name
-    domain       = var.domain
-    region       = var.aws_region
-  })
-}
-
 resource "aws_iam_role_policy_attachment" "management" {
   role       = aws_iam_role.deployer.name
   policy_arn = aws_iam_policy.management.arn
-}
-
-resource "aws_iam_role_policy_attachment" "static_content" {
-  role       = aws_iam_role.deployer.name
-  policy_arn = aws_iam_policy.static_content.arn
-}
-
-resource "aws_iam_role_policy_attachment" "backend" {
-  role       = aws_iam_role.deployer.name
-  policy_arn = aws_iam_policy.backend.arn
 }
 
