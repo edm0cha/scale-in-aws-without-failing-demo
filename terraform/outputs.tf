15c08c8678c8a1785eedacc5c979ad28f4e92e3c
Edwin Moedano <edwin.moedano@caylent.com>
2026-08-12 00:52:27 -0600

    fix: artyllery and output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,8 +1,3 @@
-output "asg_name" {
-  description = "Name of the Auto Scaling Group"
-  value       = aws_autoscaling_group.app.name
-}
-
 output "alb_dns_name" {
   description = "DNS name of the Application Load Balancer"
   value       = aws_lb.app.dns_name
